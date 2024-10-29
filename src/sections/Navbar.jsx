6327c7bce4afd79899dcedb946324c787ffeafe1
zafar-m3n <thezafar.m3n@gmail.com>
2024-10-29 20:27:03 +0530

    first commit

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import {navLinks} from "../constants/index.js";
+
+const NavItems = () => {
+    return (
+        <ul className="nav-ul">
+            {navLinks.map(({id, href, name}) => (
+                <li key={id} className="nav-li">
+                    <a href={href} className="nav-li_a" onClick={()=>{}}>{name}</a>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen((prev) => !prev);
+    return (<header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+        <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-center py-5 mx-auto c-space">
+                <a href="/" className="text-neutral-400 text-xl font-bold hover:text-white transition-colors">Zafarullah
+                    Naushad</a>
+                <button onClick={toggleMenu}
+                        className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+                        aria-label="Toggle Menu"
+                >
+                    <img src={isOpen ? "/assets/close.svg" : "/assets/menu.svg"} alt="Toggle Menu"
+                         className="w-6 h-6"
+                    />
+                </button>
+                <nav className="hidden sm:flex">
+                    <NavItems/>
+                </nav>
+            </div>
+        </div>
+        <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"} `}>
+            <nav className="p-5">
+                <NavItems/>
+            </nav>
+        </div>
+    </header>)
+}
+export default Navbar
